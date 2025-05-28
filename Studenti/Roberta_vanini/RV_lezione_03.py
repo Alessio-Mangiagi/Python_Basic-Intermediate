fdc54a789b845b69d2c59bfa0186aa6a8d91f8c7
Roberta Vanini <roberta.vnn@libero.it>
2025-05-28 18:05:13 +0200

    update lezione 3

--- Studenti/Roberta_vanini/RV_lezione_03.py
+++ Studenti/Roberta_vanini/RV_lezione_03.py
@@ -1,8 +1,67 @@
-# Lista
+# Tipologie di elementi
+
+lista = [1, 2, 3, 4] # Ordinata, permette duplicati, modificabile, indicizzabile.
+tupla = (1, 2, 3, 4) # Ordinata, permette duplicati, NON modificabile, indicizzabile.
+dizionario = {2:"a", 1:"b", 4:"c", 3:"d"} # NON ordinato, NON permette duplicati, modificabile, indicizzabile.
+# ogni elemento di un dizionario è associato ad un indice.
+# possiamo iterare un dizionario per creare un dataframe con pandas -> for key in dict:
+set = {4, 9, 8, 2} # NON ordinato, NON permette duplicati, modificabile, NON indicizzabile.
+
+print(f"set: {set} \nlista: {lista[0]} \ntupla: {tupla[0]} \ndizionario: {dizionario[1]}")
+# possiamo richiamare il primo elemento per lista e tupla. I set non sono indicizabili. Il dizionario ha un suo indice.
+
+# For loop: usando un range() itera una funzione che forniamo noi per i volte
 
 studenti = ["Federico", "Gianluca", "Massimiliano", "Roberta", "Alessio", "Antonio", "Stefano", "Mauro",
             "Costanza", "Antonio", "Mariagiada", "Annalisa"]
 
+# Il comando len() ci permette di contare elementi di una lista o lettere di una stringa. Comincia a contare da 1.
+
+len("Roberta") # per una stringa ci ridà il numero di lettere
+len(studenti) # per una lista ci fornisce il numero di elementi
+
+# Se proviamo a dare len(10) ci dà errore perché i numeri non hanno len.
+# Come facciamo a contare le cifre in un numero? Bisogna trasformarlo in stringa.
+
+x = 238271.32
+len(str(x))-1 # ATTENZIONE: ci da 9 perchè conta anche il punto
+
+for i in range(len(studenti)):
+    print("Studente numero", i + 1, ":", studenti[i])
+
+# range con un salto di 2 posizioni
+
+for i in range(0,10,2):
+    print(i)
+
+# partiamo dal 3o numero
+
+for i in range(3,5):
+    print(i)
+
+# stampiano le lettere di una parola
+
+parola = "Hello!"
+
+for i in parola:
+    print(i)
+
+# stampiamo le lettere di una parola in ogni riga e dare un numero usiamo enumerate()
+
+for indice, lettera in enumerate(parola):
+    print(indice+1, lettera)        # indice+1 perché altrimenti comincia a contare da 0
+
+###### CICLO WHILE: simile a for ma rimane attivo finché la condizione è vera
+# PERICOLOSO perché potrebbe continuare a girare finché la macchina non crasha
+# bisogna implementare delle condizioni di sicurezza per assicurarci che il ciclo si fermi.
+# un cliclo while in loop infinito che effettua una chiamata ad un server può trasformarsi in un attacco DOS!
+
+x = 0
+while x in range(10):
+    x = x+1
+    print(x)
+
+#########################################################
 # 1. Esercizio: determinare se un numero è pari o dispari
 
 def pari_dispari(num):
