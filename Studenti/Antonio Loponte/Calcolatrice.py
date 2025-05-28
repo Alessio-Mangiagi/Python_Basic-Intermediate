3f6e305db666f8313f83f2ce27cbb8d76444334f
Antonio Loponte <loponte.antonio88@gmail.com>
2025-05-28 11:21:05 +0200

    ho fatto un programma di pari e dispari

--- Studenti/Antonio Loponte/Calcolatrice.py
+++ Studenti/Antonio Loponte/Calcolatrice.py
@@ -29,15 +29,4 @@ def calcolatrice(numero_uno, numero_due,numero_operazione):
 
 calcolatrice(numero_uno, numero_due, numero_operazione)
 
-# controllo numeri pari e dispari
-
-numero = input("Inserisci un numero per verificare se è pari o dispari: ")
-
-def pari_dispari(numero):
-    if int(numero) % 2 == 0:
-        print(f"{numero} è un numero pari.")
-    else:
-        print(f"{numero} è un numero dispari.") 
-
-pari_dispari(numero)
 
