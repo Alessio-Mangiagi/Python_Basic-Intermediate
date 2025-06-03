cdfc3574c5042bbc173b8402310e81011ad06a8f
gianluca <gianlucamoriconi08@gmail.com>
2025-06-03 10:56:24 +0200

    aggiunto supporto file csv

--- Studenti/Gianluca Moriconi/le06.py
+++ Studenti/Gianluca Moriconi/le06.py
@@ -1,5 +1,5 @@
 #esercizio 1
-
+"""
 lista_nomi = []
 
 def ins_nomi():
@@ -21,3 +21,47 @@ print(nomi_miei)
 
 nome_tolto = nomi_miei.pop(0)
 print(f"Il nome tolto è: {nome_tolto}")
+"""
+
+# ----------------------------------------------------operazioni con file------------------------------------------
+"""
+with open("file.txt", "w", encoding="utf-8") as file:               #apre il file in modalità scrittura, se non esiste lo crea, con codifica utf-8 
+    file.write("Ciao, questo è un file di testo.\n")                #scrive una riga nel file
+    file.close()                                                    #chiude il file, ma non è necessario in questo caso perché il blocco with lo chiude automaticamente
+
+righe= ["Prima riga del file.\n", "Seconda riga del file.\n", "Terza riga del file.\n"]
+
+with open("file.txt", "a", encoding="utf-8") as file:               #apre il file in modalità append, per aggiungere contenuto senza sovrascrivere
+    file.write("Aggiungo un'altra riga al file.\n")  
+    file.writelines(righe)                                          #aggiunge più righe al file
+    file.flush()                                                    #forza la scrittura su disco, ma non è necessario in questo caso perché il blocco with lo fa automaticamente
+    
+    file.close()  
+
+with open("file.txt", "r", encoding="utf-8") as file:               #apre il file in modalità lettura
+    
+    contenuto = file.read()                                         #legge tutto il contenuto del file
+    #righelista = (file.readlines())                                #legge il file riga per riga e le mette in una lista
+    
+    print(contenuto)                                                #stampa il contenuto del file
+    #print(righelista)                                              #stampa la lista delle righe lette
+
+    #print(file.readline())                                         #legge e stampa la prima riga del file
+    
+    file.close()  
+"""
+#-----------------------------------------------------
+import csv  # Importa il modulo csv per lavorare con i file CSV
+
+listastud_01 = [["Gianluca", "Neri", 33], 
+                ["Luca", "Rossi", 30],
+                ["Anna", "Bianchi", 22]] 
+
+
+with open("studenti.csv", "w", encoding="utf-8", newline="") as file: 
+    writer = csv.writer(file)  # Crea un oggetto writer per scrivere nel file CSV       
+    writer.writerow("Nome,Cognome,Età\n")  # Scrive l'intestazione del CSV
+    writer.writerow("Gianluca,Moriconi,25\n")
+    writer.writerow("Luca,Rossi,30\n")
+    writer.writerow("Anna,Bianchi,22\n")
+    writer.writerows(listastud_01)  # Scrive le righe della lista nel CSV
