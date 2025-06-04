4dfcf55038197cc80a6a9c3d8c0770229522f3fa
gianluca <gianlucamoriconi08@gmail.com>
2025-06-04 12:39:46 +0200

    aggiunta classe animale

--- Studenti/Giada Carrabba/famo schifo/classe.py
+++ Studenti/Giada Carrabba/famo schifo/classe.py
@@ -1,17 +1,27 @@
-class gatto: 
-    def __init__(self, nome, eta):
+class animale:
+    def __init__(self, nome, eta, colore):
         self.nome = nome
-        self.eta = eta 
+        self.eta = eta
+        self.colore = colore
+        
+
+
+class gatto(animale): 
+    
+        
     def miagola(self):
-        print(" il gatto " + self.nome + " miagola ")
-ilmiogatto = gatto (" pippo ", " 10 ")
+        print("il gatto " + self.nome + " miagola ed ha " + self.eta + " anni e il suo colore è " + self.colore)
+
+ilmiogatto = gatto (" pippo ", " 10 " , " nero ")
 ilmiogatto.miagola()  
 
-class cane:
-    def __init__(self,nome, razza):
-        self.nome = nome
-        self.razza = razza 
+class cane(animale):
+    def __init__(self, nome, eta, colore, razza):
+        super().__init__(nome, eta, colore)
+        self.razza = razza
     def abbaia(self):
-        print ("il cane " + self.nome + " abbaia e di razza " + self.razza)
-ilmiocane = cane(" ulysse ", " pastore tedesco ")
+        print("il cane " + self.nome + " abbaia ed ha " + str(self.eta) + " anni e la sua razza è " + self.razza)
+
+ilmiocane = cane(" ulysse ", 5 , " marrone ", " labrador ")
 ilmiocane.abbaia()
+
