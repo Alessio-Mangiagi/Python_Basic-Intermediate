69befd12dde29b7ec5619289de4722e97cd5827b
MarfiaX <157723570+MarfiaX@users.noreply.github.com>
2025-06-05 15:47:02 +0200

    allora

--- Studenti/Mauro Marfia/Mauro.py
+++ Studenti/Mauro Marfia/Mauro.py
@@ -1,3 +1,10 @@
-stanze = ["camera", "cucina", "salotto", "bagno", "camera da letto"]
-tre_prese = [s for s in stanze if len(s) <= 5]
-print(tre_prese)
+#stanze = ["camera", "cucina", "salotto", "bagno", "camera da letto"]
+#tre_prese = [s for s in stanze if len(s) <= 5]
+#quattro_prese = [s for s in stanze if len(s) == 6]
+#cinque_prese = [s for s in stanze if len(s) >= 7]
+#list_c = [tre_prese, quattro_prese, cinque_prese]
+#list = [p for p in list_c]
+
+
+
+#2.
