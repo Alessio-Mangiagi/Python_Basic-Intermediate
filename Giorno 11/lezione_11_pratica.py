253a9a4d767451a88e8e063a3f6635f1545facf5
nh-spipipitone <spipitone@nohold.com>
2025-06-10 15:16:03 +0200

    Fix formatting and scrollbar behavior in the listbox section

--- Giorno 11/lezione_11_pratica.py
+++ Giorno 11/lezione_11_pratica.py
@@ -40,15 +40,15 @@ label_cognome.pack(pady=10, padx=10, anchor=tk.W)
 entry_cognome = tk.Entry(sezione_body, font=("Arial", 14), textvariable=cognome_var)
 entry_cognome.pack(pady=10, padx=10, fill=tk.X)
 
-#sezione listbox
+# sezione listbox
 sezione_testo = tk.Frame(sezione_body, bg="lightblue")
 sezione_testo.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
 # text
 test_1 = tk.Text(sezione_testo, font=("Arial", 14), bg="#e20a0a", height=10, width=50)
-scrollbar = tk.Scrollbar(sezione_testo, command=test_1.yview, fill=tk.Y)
+scrollbar = tk.Scrollbar(sezione_testo, command=test_1.yview)
 test_1.config(yscrollcommand=scrollbar.set)
-test_1.grid(column=0, row=1) 
-scrollbar.grid(column=1 , row=1)
+test_1.grid(column=0, row=1)
+scrollbar.grid(column=1, row=1, sticky="ns")
 
 
 root.mainloop()
