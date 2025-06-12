205dde42ccb90d9bd18b6c996098e1a3df603e91
nh-spipipitone <spipitone@nohold.com>
2025-06-12 12:17:10 +0200

    Refactor Paint app: replace toolbar buttons with a menu and add keyboard shortcuts

--- Giorno 13/04_menu_add_command.py
+++ Giorno 13/04_menu_add_command.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class MenuAddCommandApp:
     def __init__(self):
         self.root = tk.Tk()
@@ -9,16 +10,20 @@ class MenuAddCommandApp:
         filemenu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="File", menu=filemenu)
         filemenu.add_command(label="Apri", command=self.apri)
-        filemenu.add_command(label="Salva", command=self.salva)
+        filemenu.add_command(label="Salva", command=self.salva, accelerator="Ctrl+S")
         self.root.geometry("350x150")
         self.label = tk.Label(self.root, text="Clicca su Apri o Salva nel menu File")
         self.label.pack(pady=40)
+
     def apri(self):
         self.label.config(text="Hai cliccato Apri")
+
     def salva(self):
         self.label.config(text="Hai cliccato Salva")
+
     def run(self):
         self.root.mainloop()
+
 
 if __name__ == "__main__":
     app = MenuAddCommandApp()
