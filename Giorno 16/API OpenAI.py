f1f0d0f5ac2762ecd1990080313cd9338ac37f27
nh-spipipitone <spipitone@nohold.com>
2025-06-18 09:36:33 +0200

    Refactor OpenAI API integration: update API key handling and modify chat completion parameters

--- Giorno 16/API OpenAI.py
+++ Giorno 16/API OpenAI.py
@@ -1,15 +1,20 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 client = OpenAI(
-  api_key="..."
+    api_key=os.getenv("OPENAI_API_KEY"),
 )
 
 completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
-  messages=[
-    {"role": "user", "content": "write a haiku about ai"}
-  ]
+    model="gpt-4.1-mini",
+    messages=[
+        {"role": "user", "content": "ci ucciderai tutti?"}
+    ],  # role può essere "system", "user" o "assistant"
 )
 
-print(completion.choices[0].message);
+print(completion.choices[0].message)
+print(completion.choices[0].message.content)
